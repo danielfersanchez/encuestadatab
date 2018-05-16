b48a668235d07eebe3c4189c089e296e1d93ae2f
danielfersanchez <38057662+danielfersanchez@users.noreply.github.com>
2018-05-15 20:45:09 -0500

    comit

---
+++ encuestadatab/encuestadatab/lista.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace encuestadatab
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class lista : ContentPage
+	{
+		public lista ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
